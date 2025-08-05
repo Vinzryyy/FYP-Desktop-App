348a0a27e886ef8d67955665297cb7c3eaef3151
Lesterchoo709 <lesterchooyouliang@gmail.com>
2025-08-06 00:53:48 +0800

    Add files via upload
    Firebase tab to load, save and delete profile

---
+++ Firebase Tab to save and load profile/Models/DeviceProfile.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using System.Windows.Media;
+
+namespace FYP.Models
+{
+    public class DeviceProfile : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public int SelectedProfileId { get; set; } // Links to InputProfile.Id
+
+        private string _firebaseKey;
+        public string FirebaseKey
+        {
+            get => _firebaseKey;
+            set { _firebaseKey = value; OnPropertyChanged(); }
+        }
+
+        // Read-only property for display
+        public string ProfileTypeDisplay => SelectedProfileNum switch
+        {
+            "1" => "Keyboard n Mouse",
+            "2" => "Xbox emulation",
+            "3" => "DS4 emulation",
+            _ => "Unknown"
+        };
+
+        private string _profileName;
+        public string ProfileName
+        {
+            get => _profileName;
+            set { _profileName = value; OnPropertyChanged(); }
+        }
+
+        private string _dateCreated;
+        public string DateCreated
+        {
+            get => _dateCreated;
+            set { _dateCreated = value; OnPropertyChanged(); }
+        }
+
+        private string _lastUpdated;
+        public string LastUpdated
+        {
+            get => _lastUpdated;
+            set { _lastUpdated = value; OnPropertyChanged(); }
+        }
+
+        private string _deviceName;
+        public string DeviceName
+        {
+            get => _deviceName;
+            set { _deviceName = value; OnPropertyChanged(); }
+        }
+
+        private string _status;
+        public string Status
+        {
+            get => _status;
+            set { _status = value; OnPropertyChanged(); }
+        }
+
+        private InputProfile _selectedProfile;
+        public InputProfile SelectedProfile
+        {
+            get => _selectedProfile;
+            set { _selectedProfile = value; OnPropertyChanged(); }
+        }
+        
+        private string _selectedProfileNum;
+        public string SelectedProfileNum
+        {
+            get => _selectedProfileNum;
+            set { _selectedProfileNum = value; OnPropertyChanged();
+                    OnPropertyChanged(nameof(ProfileTypeDisplay)); // Update display text
+             }
+        }
+
+        private bool _isLinked;
+        public bool IsLinked
+        {
+            get => _isLinked;
+            set { _isLinked = value; OnPropertyChanged(); OnPropertyChanged(nameof(LinkedBrush)); }
+        }
+
+        private bool _needsUpdate;
+        public bool NeedsUpdate
+        {
+            get => _needsUpdate;
+            set { _needsUpdate = value; OnPropertyChanged(); }
+        }
+
+        [JsonIgnore]
+        public Brush LinkedBrush => IsLinked ? Brushes.Green : Brushes.Red;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Firebase Tab to save and load profile/Models/InputProfile.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYP.Models
+{
+    public class InputProfile : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        private string _profileName;
+        private string _dateCreated;
+        private string _lastUpdated;
+
+        public string ProfileName
+        {
+            get => _profileName;
+            set { _profileName = value; OnPropertyChanged(nameof(ProfileName)); }
+        }
+
+        public string DateCreated
+        {
+            get => _dateCreated;
+            set { _dateCreated = value; OnPropertyChanged(nameof(DateCreated)); }
+        }
+
+        public string LastUpdated
+        {
+            get => _lastUpdated;
+            set { _lastUpdated = value; OnPropertyChanged(nameof(LastUpdated)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
